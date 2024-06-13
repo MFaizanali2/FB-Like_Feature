3fd77c60bf07fd352d2706889bc61a17fb0d842b
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-06-13 12:39:36 +0500

    js line 18
    continue

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 
+let icon = document.getElementById("icon");
+let down = document.getElementById("down");
+let sum =document.getElementById("sum")
+let sum1 =document.getElementById("sum1")
+let count = 0;
+let isshow = false;
 function change(){
-    let icon = document.getElementById("icon").value;
-    icon.classList.add("ali");
+    isshow = !isshow;
+    if(isshow){
+        icon.classList.add("ali")
+        sum.innerText = count + 1;
+    }else{
+        icon.classList.remove("ali")
+        sum.innerText = count + 0;
+        down.classList.add("faizi")
+        sum1.innerText = count + 1;
+    }
+    
 }
